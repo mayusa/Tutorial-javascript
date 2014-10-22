091ec54d90824bba7985306e1b70d708191a815f
May <ashucn@gmail.com>
2014-10-22 12:45:14 -0700

    createCORSRequest()
    Cross-Browser CORS

---
+++ createCORS.js
@@ -0,0 +1,21 @@
+// Cross-Browser CORS
+function createCORSRequest(method, url){
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr){
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
+}
+
+var request = createCORSRequest("get", "http://www.somewhere-else-ashu.com/page/");
+if (request){
+    request.onload = function(){
+        //do something with request.responseText
+    };
+    request.send();
+}
