0d50e47333ac4a6cf0171af35d922f38f8168f47
May <ashucn@gmail.com>
2014-10-22 23:54:21 -0700

    Cross-Browser CORS
    Cross-Browser CORS

--- createCORS.js
+++ createCORS.js
@@ -12,10 +12,29 @@ function createCORSRequest(method, url){
     return xhr;
 }
 
-var request = createCORSRequest("get", "http://www.somewhere-else-ashu.com/page/");
+var request = createCORSRequest("get", 
+    "http://www.somewhere-else-ashu.com/page/");
 if (request){
     request.onload = function(){
         //do something with request.responseText
     };
     request.send();
 }
+
+
+            var img = new Image();
+            img.onload = img.onerror = function(){
+                console.log("Done!");
+            };
+            img.src = "http://www.baidu.com";
+
+//JSONP通过动态<script>元素来使用。为src属性指定一个跨域URL
+//JSONP从其他域中加载代码执行
+// callback({ “name”: “Nicholas” });
+function handleResponse(response){
+    console.log("You’re at IP address " + response.ip + ", which is in " + 
+          response.city + ", " + response.region_name);
+}
+var script = document.createElement("script");
+script.src = "http://freegeoip.net/json/?callback=handleResponse";
+document.body.insertBefore(script, document.body.firstChild);
