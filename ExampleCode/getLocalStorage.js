1b73d1d74d4860ea351de13073af41b3ed495d1c
May <ashucn@gmail.com>
2014-10-23 17:32:23 -0700

    getLocalStorage()
    To equalize for browsers that support only globalStorage

---
+++ ExampleCode/getLocalStorage.js
@@ -0,0 +1,10 @@
+//To equalize for browsers that support only globalStorage
+function getLocalStorage(){
+    if (typeof localStorage == "object"){
+        return localStorage;
+    } else if (typeof globalStorage == "object"){
+        return globalStorage[location.host];
+    } else {
+        throw new Error("Local storage not available.");
+    }
+}
