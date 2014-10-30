d61a744926b959c9957c98c30550c75ad36a2e41
May <ashucn@gmail.com>
2014-10-30 00:01:07 -0700

    update comment

--- ExampleCode/AjaxForm.js
+++ ExampleCode/AjaxForm.js
@@ -1,8 +1,10 @@
 // Passing Data to jQuery’s Ajax Utilities (POST request)
 $("#submit_button").click(function(event) {
-    $(this.form).load(       			// Replace the form by loading...
-        this.form.action,           	// the form url
-        $(this.form).serialize());  	// with the form data appended to it
-    event.preventDefault();   			// Don't do the default form submission
+	var fm = $(this.form).serialize();
+	alert(fm);// e.g.  name=abc&pwd=123
+    $(this.form).load(       		// Replace the form by loading...
+        this.form.action,            	// the form url
+        fm);  	// with the form data appended to it
+    event.preventDefault();   		// Don't do the default form submission
     this.disabled = "disabled";     	// Prevent multiple submissions
 });
