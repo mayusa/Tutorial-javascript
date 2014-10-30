484aa17a6d072fed3015861dab410b778a2b7817
May <ashucn@gmail.com>
2014-10-30 00:26:56 -0700

    update function

--- ExampleCode/AjaxForm.js
+++ ExampleCode/AjaxForm.js
@@ -1,7 +1,8 @@
-// Passing Data to jQuery’s Ajax Utilities (POST request)
+// Passing Data to jQuery’s Ajax Utilities (GET request)
+// get the infomation in PHP: echo $_GET['username'];
 $("#submit_button").click(function(event) {
-	var fm = $(this.form).serialize();
-	alert(fm);// eg.  "name=abc&pwd=123"
+        var fm = $(this.form).serialize();
+        alert(fm);// eg.  "username=abc&password=123"
     $(this.form).load(       		// Replace the form by loading...
         this.form.action,            	// the form url
         fm);  	// with the form data appended to it
